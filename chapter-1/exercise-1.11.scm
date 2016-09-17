ba6acab5a07ee37d3877dfaf90d5fe9235debdf9
jiakai0419 <jiakai0419@gmail.com>
2016-09-17 17:51:52 +0800

    exercise-1.11

---
+++ chapter-1/exercise-1.11.scm
@@ -0,0 +1,18 @@
+#lang scheme
+
+;recursion
+(define (fr n)
+  (cond ((< n 3) n)
+        (else (+ (fr (- n 1))
+                 (* 2 (fr (- n 2)))
+                 (* 3 (fr (- n 3)))))))
+
+
+;iteration
+(define (fi n)
+  (define (fi-iter a b c t)
+    (if (= t 0)
+      c
+      (fi-iter b c (+ c (* 2 b) (* 3 a)) (- t 1))))
+  (cond ((< n 3) n)
+        (else (fi-iter 0 1 2 (- n 2)))))
