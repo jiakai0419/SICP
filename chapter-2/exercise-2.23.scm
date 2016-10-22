91a2107e58f079920c5df7858e3a08d537b72ca0
jiakai0419 <jiakai0419@gmail.com>
2016-10-23 00:46:02 +0800

    exercise-2.23

---
+++ chapter-2/exercise-2.23.scm
@@ -0,0 +1,14 @@
+#lang scheme
+
+(define (for-each f l)
+  (if (null? l)
+    (newline)
+    ((lambda (l)
+       (f (car l))
+       (for-each f (cdr l)))
+     l)))
+
+;;; test
+(for-each (lambda (x) (newline) (display x))
+          (list 57 321 88))
+
