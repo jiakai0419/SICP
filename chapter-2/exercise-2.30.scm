aecbfea5ba140601f2c858d29cd790d050419c01
jiakai0419 <jiakai0419@gmail.com>
2016-11-12 02:10:20 +0800

    clarify

--- chapter-2/exercise-2.30.scm
+++ chapter-2/exercise-2.30.scm
@@ -13,12 +13,12 @@
            (* subtree subtree)))
        tree))
 
-(define square-tree square-tree-A)
+; (define square-tree square-tree-A)
 ; (define square-tree square-tree-B)
 
 ;;; test
-(square-tree
-  (list 1
-        (list 2 (list 3 4) 5)
-        (list 6 7)))
+; (square-tree
+;   (list 1
+;         (list 2 (list 3 4) 5)
+;         (list 6 7)))
 
