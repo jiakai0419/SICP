d1c2739c3baa0a8c793daa70c18e67bae401a2ba
jiakai0419 <jiakai0419@gmail.com>
2016-11-28 23:05:18 +0800

    exercise 2.59 - 2.60

---
+++ chapter-2/exercise-2.59.scm
@@ -0,0 +1,14 @@
+#lang scheme
+
+(define (element-of-set? x set)
+  (cond ((null? set) #f)
+        ((equal? x (car set)) #t)
+        (else (element-of-set? x (cdr set)))))
+
+(define (union-set s1 s2)
+  (cond ((null? s1) s2)
+        ((null? s2) s1)
+        ((element-of-set? (car s1) s2) (union-set (cdr s1) s2))
+        (else (cons (car s1)
+                    (union-set (cdr s1) s2)))))
+
---
+++ chapter-2/exercise-2.60.scm
@@ -0,0 +1,20 @@
+#lang scheme
+
+(define (element-of-set? x set)
+  (cond ((null? set) #f)
+        ((equal? x (car set)) #t)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (cons x set))
+
+(define (union-set s1 s2)
+  (append s1 s2))
+
+(define (intersection-set s1 s2)
+  (cond ((or (null? s1) (null? s2)) '())
+        ((element-of-set? (car s1) s2)
+         (cons (car s1)
+               (intersection-set (cdr s1) s2)))
+        ((intersection-set (cdr s1) s2))))
+
