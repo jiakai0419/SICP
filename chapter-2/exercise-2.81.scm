93c85fcaae684391153a02af8cef98d0603e978b
jiakai0419 <jiakai0419@gmail.com>
2016-12-18 17:41:01 +0800

    fix bug

--- chapter-2/exercise-2.81.scm
+++ chapter-2/exercise-2.81.scm
@@ -3,6 +3,9 @@
 ;;; pseudo code
 
 (define (apply-generic op . args)
+  (define (no-method type-tags)
+    (error "No method for these types"
+           (list op type-tags)))
   (let ((type-tags (map type-tag args)))
     (let ((proc (get op type-tags)))
       (if proc
@@ -13,14 +16,11 @@
                 (a1 (car args))
                 (a2 (cadr args)))
             (if (eq? type1 type2)
-              (error "No method for these types"
-                     (list op type-tags))
+              (no-method type-tags)
               (let ((t1->t2 (get-coercion type1 type2))
                     (t2->t1 (get-coercion type2 type1)))
                 (cond (t1->t2 (apply-generic op (t1->t2 a1) a2))
                       (t2->t1 (apply-generic op a1 (t2->t1 a2)))
-                      (else "No method for test types"
-                            (list op type-tags))))))
-          (error "No method for these types"
-                 (list op type-tags)))))))
+                      (else (no-method type-tags))))))
+          (no-method type-tags))))))
 
