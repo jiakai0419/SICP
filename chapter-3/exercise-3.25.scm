a89be0089d56658a8d2f16bc30a159dcfff30414
jiakai0419 <jiakai0419@gmail.com>
2017-01-03 17:30:33 +0800

    format exercise-3.25

--- chapter-3/exercise-3.25.scm
+++ chapter-3/exercise-3.25.scm
@@ -4,6 +4,7 @@
 
 (define (make-table)
   (let ((local-table (list '*table*)))
+
     (define (assoc key records)
       (cond ((null? records) #f)
             ((equal? key (caar records)) (car records))
