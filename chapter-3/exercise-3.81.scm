24a10371c37ecc12c61763d04ebf501a55669a1a
jiakai0419 <jiakai0419@gmail.com>
2017-01-21 22:33:27 +0800

    exercise-3.81

---
+++ chapter-3/exercise-3.81.scm
@@ -0,0 +1,36 @@
+#lang planet neil/sicp
+
+(#%require "stream.scm")
+
+(define (rand-update x)
+  (let ((a 1103515245)
+        (b 12345)
+        (m 2147483647))
+    (modulo (+ (* a x)
+               b)
+            m)))
+
+(define (rand-stream input-stream)
+  (define init-seed 0)
+  (define (generate-rand-stream input-stream seed)
+    (if (stream-null? input-stream)
+      the-empty-stream
+      (let ((input (stream-car input-stream)))
+        (if (eq? input 'g)
+          (let ((next (rand-update seed)))
+            (cons-stream next
+                         (generate-rand-stream (stream-cdr input-stream) next)))
+          (generate-rand-stream (stream-cdr input-stream) input)))))
+  (generate-rand-stream input-stream init-seed))
+
+;;; test
+(define istream
+  (cons-stream 1024
+               (cons-stream 'g
+                            (cons-stream 'g
+                                         (cons-stream 'g
+                                                      (cons-stream 'g
+                                                                   (cons-stream 'g
+                                                                                istream)))))))
+(print-stream (rand-stream istream) 13)
+
